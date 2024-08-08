3bfefabfbd76d1cd80660795ba5b7985b2f64fef
dinhphap2003 <phapcv2003@gmail.com>
2024-08-08 21:54:16 +0700

    first commit

--- .history/movie-project/vite.config_20240808215154.ts
+++ .history/movie-project/vite.config_20240808215154.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./",
-  build: {
-    outDir: "dist",
-  },
-  publicDir: "public",
   plugins: [react()],
-  base: "/Movie",
+  base:"/Movie",
 })
